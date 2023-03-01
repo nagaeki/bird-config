7e608aabd82bbbc0f465029ff4117986bdd0ccc2
eki <m@eki.moe>
2023-03-01 22:06:55 +0800

    Update update.sh

--- update.sh
+++ update.sh
@@ -6,4 +6,5 @@ cp /root/bird-config/bird.conf /etc/bird/bird.conf
 cp /root/bird-config/dn42/main.conf /etc/bird/dn42/main.conf
 cp /root/bird-config/dn42/functions.conf /etc/bird/dn42/functions.conf
 cp /root/bird-config/dn42/templates.conf /etc/bird/dn42/templates.conf
+cp /root/bird-config/global_check.conf /etc/bird/global_check.conf
 birdc c
