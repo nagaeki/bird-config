40863ee63dc46a9782105e1aa070e061b61d84ba
eki <m@eki.moe>
2023-06-17 19:50:51 +0900

    include install script

---
+++ install-bird.sh
@@ -0,0 +1,13 @@
+
+#!/bin/bash
+apt update
+apt install -y build-essential autoconf git flex bison m4 libssh-dev libncurses-dev libreadline-dev
+cd
+git clone https://gitlab.nic.cz/labs/bird.git -b v2.13
+cd bird
+autoreconf
+./configure --prefix= --sysconfdir=/etc/bird --runstatedir=/var/run/bird
+make && make install
+rm -r /root/bird/
+wget https://github.com/nagaeki/bird-config/raw/main/bird.service -O /etc/systemd/system/multi-user.target.wants/bird.service
+systemctl enable bird.service --now
