c5f5e9607172429f4c8ee13e2d1b3e38c612f283
eki <i@eki.moe>
2023-07-19 23:13:51 +0900

    Update install-bird.sh

--- install-bird.sh
+++ install-bird.sh
@@ -1,6 +1,7 @@
 
 #!/bin/bash
 apt update
+apt install bird2 -y
 apt install -y build-essential autoconf git flex bison m4 libssh-dev libncurses-dev libreadline-dev
 cd
 git clone https://gitlab.nic.cz/labs/bird.git -b v2.13
@@ -9,5 +10,4 @@ autoreconf
 ./configure --prefix= --sysconfdir=/etc/bird --runstatedir=/var/run/bird
 make && make install
 rm -r /root/bird/
-wget https://github.com/nagaeki/bird-config/raw/main/bird.service -O /etc/systemd/system/bird.service
 systemctl enable bird.service --now
