33f1c43f1cfdc8a10de53af59190b46a4d75fbe7
eki <i@eki.moe>
2025-04-23 15:00:36 +0900

    Update update-bird.sh to v2.17

--- update-bird.sh
+++ update-bird.sh
@@ -3,7 +3,7 @@ apt update
 apt install -y build-essential autoconf git flex bison m4 libssh-dev libncurses-dev libreadline-dev
 cd
 rm -r bird
-git clone https://gitlab.nic.cz/labs/bird.git -b v2.14
+git clone https://gitlab.nic.cz/labs/bird.git -b v2.17
 cd bird
 autoreconf
 ./configure --prefix= --sysconfdir=/etc/bird --runstatedir=/var/run/bird
